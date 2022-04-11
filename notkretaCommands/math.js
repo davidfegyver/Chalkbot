f0be2a5d1e94c0668d045219d5192464d2242961
David Fegyver <fegyverdavid.bator@gmail.com>
2022-04-11 15:52:31 +0200

    Added Jinja's AntiSkid System
    haha, fixed multiple mathjs vulnerabilities

--- notkretaCommands/math.js
+++ notkretaCommands/math.js
@@ -1,20 +1,52 @@
 const Discord = require("discord.js");
-const math = require("mathjs");
-exports.run = (client, message, args) => {
-  if (!args.length)
-    return message.channel.send(
-      `Adj meg egy műveletet amit meg akarsz oldani.`
-    );
-  let whatto = args.join(" ");
-  try {
-    let result = math.evaluate(whatto).toString();
+const { StaticPool,isTimeoutError } = require("node-worker-threads-pool");
 
+exports.run = async (client, message, args) => {
+  if (!args.length) return message.channel.send("Adj meg egy műveletet!");
+  let expression = args.join(" ");
+
+  
+  try {
+    const result = await calculate(expression);
     const embed = new Discord.MessageEmbed()
       .setTitle("Matek Megoldás")
-      .addField(`Feladat:`, whatto, true)
+      .addField(`Feladat:`, expression, true)
       .addField(`Eredmény:`, result, true);
-    message.channel.send({ embed });
+    message.channel.send(embed);
   } catch (e) {
-    return message.channel.send("Szerintem ez nem megoldható.");
+    if(isTimeoutError(e)) return message.channel.send("Your crash attempt has been intercepted by Jinja's AntiSkid-system. Better luck next time!")
+    return message.channel.send("Hiba történt a feldolgozás közben");
   }
 };
+
+function calculate(expression) {
+  const pool = new StaticPool({
+    size: 4,
+    task: (expression) => {
+      function blocked() {
+        throw new Error();
+      }
+      const { create, all } = require("mathjs");
+      const math = create(all);
+      const limitedEvaluate = math.evaluate;
+
+      math.import(
+        {
+          import: blocked,
+          createUnit: blocked,
+          evaluate: blocked,
+          parse: blocked,
+          simplify: blocked,
+          derivative: blocked,
+        },
+        { override: true }
+      );
+      return limitedEvaluate(expression)
+    },
+  });
+  return pool
+    .createExecutor()
+    .setTimeout(1000)
+    .exec(expression)
+    .then((result) => JSON.stringify(result._data || result));
+}
